30115e89ef65acd06f97ae95dab02fa7d5a6216b
Harsh <harsh2005dalal@gmail.com>
2025-08-04 03:37:16 +0530

    Deployment issues

---
+++ src/components/ProjectDetails.jsx
@@ -0,0 +1,23 @@
+// import { p } from 'framer-motion/client'
+// import React from 'react'
+
+// const ProjectDetails = ({title,description,subDescription, image, tags, href}) => {
+//   return ( 
+//     <div className='fixed inset-0 z-50 flex items-center justify-center w-full h-full overflow-hidden backdrop-blur-sm'>
+//       <div className='relative max-w-2xl border shadow-sm rounded-2xl bg-gradient-to-l from-midnight to-navy border-white/10'></div>
+//       <button className='absolute p-2 rounded-sm top-5 right-5 bg-midnight hover:bg-gray-500'>
+//         <img src="assets/close.svg" alt="" className='w-6 h-6' />
+//       </button>
+//       <img src={image} alt={title} className='inset-0 rounded-t-2xl' />
+//       <div className='p-5'>
+//         <h5 className='mb-2'>{title}</h5>
+//         <p>{description}</p>
+//         {subDescription.map((subDesc, index)=>(
+//             <p>{subDesc}</p>
+//         ))}
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default ProjectDetails
---
+++ src/sections/Projects.jsx
@@ -0,0 +1,17 @@
+import { section } from "framer-motion/client"
+import { myProjects } from "../constants"
+import Project from "../components/Project"
+
+const Projects = () => {
+  return (
+    <section className="relative c-space section-spacing">
+        <h2 className="text-heading">My Selected Projects</h2>
+        <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent mt-12 h-[1px] w-full"></div>
+        {myProjects.map((project)=>(
+            <Project key={project.id} {...project}/>
+        ))}
+    </section>
+  )
+}
+
+export default Projects
