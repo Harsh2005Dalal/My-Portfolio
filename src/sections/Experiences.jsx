fcaba437405410168c0c6aed43b8467eb674065f
Harsh <harsh2005dalal@gmail.com>
2025-08-05 16:32:17 +0530

    Deployment Issues v5

--- src/sections/Experiences.jsx
+++ src/sections/Experiences.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Timeline } from '../components/timeline';
+import { Timeline } from '../components/Timeline';
 import { experiences } from '../constants';
 const Experiences = () => {
   return (
